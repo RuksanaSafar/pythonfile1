8dee9c9f10998d9664354a9d2cf2e54d95081cb6
RuksanSafar <ruksanasafar2609@gmail.com>
2024-05-29 16:28:24 +0530

    string.py

---
+++ string.py
@@ -0,0 +1,49 @@
+a="apple"
+print(a)
+print(type(a))
+print(len(a))
+print(a[1])
+for i in a:
+    print(i)
+b="orange is a fruit"  
+print(b) 
+print(len(b))
+print(b[7])
+print("is" in b)
+print("and" in b)
+print("fruit" not in b)
+print("boy" not in b )
+print(b[2:5])
+print(b[-1])
+print(b[3:])
+print(b[:9])
+print(b[-2])
+print(b[-6:-3])
+print(b.upper())
+print(b.lower())
+c="manu is a tall boy and \n he has height {} "
+d=134
+print(c)
+print(c.strip())
+print(c.replace("m","s"))
+print(c.split(","))
+print(b+c)
+print(c.format(d))
+z= "i want {} peace of item number {} for {}rs"
+x=3
+y=567
+m=20
+print(z.format(x,y,m))
+print(c.count("a"))
+print(30>40)
+print(40>30)
+print(30==30)
+x=20
+y=5
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x//y)
+print(x**y)
