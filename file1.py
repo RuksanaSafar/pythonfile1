767f4a1dcf0323508751b4d3b1d11771b82321ad
RuksanSafar <ruksanasafar2609@gmail.com>
2024-05-29 10:30:31 +0530

    first commit

---
+++ file1.py
@@ -0,0 +1,18 @@
+print("Welcome")
+
+#print("Hello")
+""" my name is ruksana 
+i completed btech
+i love my family """
+x=10
+print(x)
+z="flower"
+print(z)
+_age=25
+print(_age)
+x,y,z=10,20,30
+print(x)
+print(y)
+print(z)
+x=y=z=10
+print(y)
