4305cd26a24e7432bf04491cfee69e6d9bd27b87
RuksanSafar <ruksanasafar2609@gmail.com>
2024-05-31 14:45:22 +0530

    string.py

--- example2.py
+++ example2.py
@@ -1,4 +1,4 @@
-a="john is a good boy"
+a="john {} is a good{} boy{} "
 print(len(a))
 print(a[5])
 print("good" in a)
@@ -7,3 +7,24 @@ print(a[2:7])
 print(a[-9:-3])
 print(a[-5:])
 print(a[:-8])
+print(a)
+print(type(a))
+print(len(a))
+print(a[8])
+print("is" in a)
+print("was" is not a)
+print(a.replace("j","h"))
+print(a.strip())
+print(a.count("o"))
+print(a.split(","))
+print(a[:-7])
+print(a[:-10])
+print(a[3:8])
+print(a[-4:])
+print(a[-3:-8])
+x=20
+y=30
+z=40
+print(a.format(x,y,z))
+
+
